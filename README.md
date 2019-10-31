b061aee5baf519083c5d54552fb558fc3400c8d2
Melxi <magomed.futijev@gmail.com>
2019-10-31 13:31:57 +0200

    user login with token authentication in backend

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 | day | time | task  |
 | :----:|:-----| :-----|
 | 1.10. | 1    | initialized project and created navbar component |
-| oa   | 1   | | 
+| 5.10. | 1    | setup mongoose and created userSchema            |
+| 26.10.| 1    | user creation and validation in backend          |
+| oa    | 1    | | 
--- vuokra-asunnot/server/app.js
+++ vuokra-asunnot/server/app.js
@@ -6,10 +6,16 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 logger.info('connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
+mongoose.connect(config.MONGODB_URI, { 
+  useNewUrlParser: true, 
+  useFindAndModify: false, 
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
   .then(() => logger.info('connected to MongoDB'))
   .catch(error => logger.error('error connecting to MongoDB:', error.message))
 
@@ -17,6 +23,7 @@ app.use(bodyParser.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.get('/', (req, res) => {
   res.send('<h1>Melxi</h1>')
